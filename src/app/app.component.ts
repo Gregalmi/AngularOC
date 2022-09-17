d3bb113bbde893f96d84a99aaf64285809ae02ba
Gregalmi <gregory.almirall@gmail.com>
2022-09-17 15:14:34 +0200

    fin deuxieme partie

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FaceSnap } from './models/face-snap.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  mySnap!: FaceSnap;
+  myOtherSnap!: FaceSnap;
+  myLastSnap!: FaceSnap;
+
+  ngOnInit(): void {
+    this.mySnap = new FaceSnap(
+      'Tête',
+      'Une drôle de tête',
+      new Date(),
+      0,
+      'https://cdn.pixabay.com/photo/2022/09/13/13/47/animal-7451969_960_720.jpg'
+    );
+    this.myOtherSnap = new FaceSnap(
+      'fleur',
+      'une jolie fleur',
+      new Date(),
+      0,
+      'https://cdn.pixabay.com/photo/2022/03/01/09/35/iceland-poppy-7040946_960_720.jpg'
+    );
+    this.myLastSnap = new FaceSnap(
+      'cat',
+      'one cate',
+      new Date(),
+      0,
+      'https://cdn.pixabay.com/photo/2022/09/06/07/49/cat-7436051_960_720.jpg'
+    );
+  }
+}
--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, Input, OnInit} from '@angular/core';
+import { FaceSnap } from '../models/face-snap.model';
 
 @Component({
   selector: 'app-face-snap',
@@ -6,33 +7,19 @@ import { Component, OnInit} from '@angular/core';
   styleUrls: ['./face-snap.component.scss']
 })
 export class FaceSnapComponent implements OnInit {
-  title!: string;
-  description!: string;
-  createdDate!: Date;
-  snaps!: number;
-  imageUrl!: string;
-  isSnap!: boolean;
+  @Input() faceSnap!: FaceSnap;
   buttonText!: string;
 
   ngOnInit() {
-    this.title = 'Tête';
-    this.description = 'Une drôle de tête';
-    this.createdDate = new Date();
-    this.snaps = 0;
-    this.imageUrl = 'https://cdn.pixabay.com/photo/2022/09/13/13/47/animal-7451969_960_720.jpg';
-    this.isSnap = false;
     this.buttonText = "Oh snap!";
-
   }
 
   onSnap() {
-    if(this.isSnap == false) {
-      this.snaps++;
-      this.isSnap = true;
+    if(this.buttonText === "Oh snap!") {
+      this.faceSnap.snaps++;
       this.buttonText = "Ooops snaps";
     } else {
-      this.snaps--;
-      this.isSnap = false;
+      this.faceSnap.snaps--;
       this.buttonText = "Oh snap!";
     }    
   }
