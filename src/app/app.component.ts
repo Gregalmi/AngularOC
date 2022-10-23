b89b087a4cd2ebd7fa922a29dac0350e737535bf
Gregalmi <gregory.almirall@gmail.com>
2022-10-23 11:44:25 +0200

    les directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,31 +7,35 @@ import { FaceSnap } from './models/face-snap.model';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  mySnap!: FaceSnap;
-  myOtherSnap!: FaceSnap;
-  myLastSnap!: FaceSnap;
+  faceSnaps!: FaceSnap[];
 
   ngOnInit(): void {
-    this.mySnap = new FaceSnap(
-      'Tête',
-      'Une drôle de tête',
-      new Date(),
-      0,
-      'https://cdn.pixabay.com/photo/2022/09/13/13/47/animal-7451969_960_720.jpg'
-    );
-    this.myOtherSnap = new FaceSnap(
-      'fleur',
-      'une jolie fleur',
-      new Date(),
-      0,
-      'https://cdn.pixabay.com/photo/2022/03/01/09/35/iceland-poppy-7040946_960_720.jpg'
-    );
-    this.myLastSnap = new FaceSnap(
-      'cat',
-      'one cate',
-      new Date(),
-      0,
-      'https://cdn.pixabay.com/photo/2022/09/06/07/49/cat-7436051_960_720.jpg'
-    );
+    this.faceSnaps = [
+      {
+        title: 'Tête',
+        description: 'Une drôle de tête',
+        createdDate: new Date(),
+        snaps: 0,
+        imageUrl: 'https://cdn.pixabay.com/photo/2022/09/13/13/47/animal-7451969_960_720.jpg',
+        location: 'Paris'
+  
+      },
+      {
+        title: 'fleur',
+        description: 'une jolie fleur',
+        createdDate: new Date(),
+        snaps: 0,
+        imageUrl: 'https://cdn.pixabay.com/photo/2022/03/01/09/35/iceland-poppy-7040946_960_720.jpg',
+        location: 'Montagne'
+  
+      },
+      {
+        title: 'cat',
+        description: 'one cate',
+        createdDate: new Date(),
+        snaps: 0,
+        imageUrl: 'https://cdn.pixabay.com/photo/2022/09/06/07/49/cat-7436051_960_720.jpg'
+      }
+    ]
   }
 }
